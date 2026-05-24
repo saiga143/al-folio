ecfd06cda70bed3d556305ed7369570254315099
saiga143 <72392083+saiga143@users.noreply.github.com>
2026-05-24 21:13:05 +0000

    Deploying to gh-pages from @ saiga143/al-folio@12975c088c8e20f30a4d1982862e410aac561436 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -338,52 +338,7 @@ ninja.data = [{
           section: "News",},{id: "news-visited-lund-university-for-a-sesac-meeting-and-strategic-planning-sessions-we-kickedoff-hackathon-planning-with-two-industry-partners-read-the-full-deatils-here-also-attended-the-inauguration-of-hägestrand-lab-read-full-details-here",
           title: 'Visited Lund University for a SESAC meeting and strategic planning sessions. We kickedoff...',
           description: "",
-          section: "News",},{id: "projects-deprimap",
-          title: 'DEPRIMAP',
-          description: "Unraveling the dynamics of deprived urban areas in the Majority World using AI and Earth Observation to foster evidence-based sustainable planning",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-analysis-of-urban-heat-island-uhi-effect-in-ahmedabad-india-city",
+          section: "News",},{id: "projects-analysis-of-urban-heat-island-uhi-effect-in-ahmedabad-india-city",
           title: 'Analysis of Urban Heat Island (UHI) effect in Ahmedabad, India city',
           description: "",
           section: "Projects",handler: () => {
